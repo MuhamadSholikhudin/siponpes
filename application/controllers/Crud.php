0ad826376fd2b6700391d486351f8c0429cfdae7
MuhamadSholikhudin <muhammadsholihudin18@gmail.com>
2021-08-22 23:28:01 +0700

    latihan crud

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -3,12 +3,63 @@ class Crud extends CI_Controller
 {
 
 
-    public function mahasiswa (){
-        $data['title'] = 'Mahasiswa';
+    public function index(){
 
-        $this->load->view('page/theme/header', $data);
-        $this->load->view('page/beranda');
-        $this->load->view('page/theme/footer');
+        $data['mahasiswa'] = $this->db->query("SELECT * FROM mahasiswa")->result(); //result() digunakan untuk menampilkan banyak baris
+        // $data di masukkan ke view
+        $this->load->view('crud/index', $data);
+    }
 
+    public function tambah()
+    {
+        // menampilkan view 
+        $this->load->view('crud/tambah');
+    }
+
+    public function aksi_tambah()
+    {
+        // $this->input->post digunakan untuk mengambil data inputan view dari name
+        $nim = $this->input->post('nim');
+        $nama = $this->input->post('nama');
+
+        // query sql $this->db->query(" isi_query ");
+        $this->db->query("INSERT INTO mahasiswa (nim, nama) VALUES ('$nim', '$nama')");
+        
+        // menuju alamat crud
+        redirect('crud');
+    }
+
+    public function ubah($nim)
+    {
+
+        $data['mahasiswa'] = $this->db->query("SELECT * FROM mahasiswa WHERE nim = '$nim' ")->row(); //row() digunakan untuk menampilkan satu baris
+        
+        // $data di masukkan ke view
+        $this->load->view('crud/ubah', $data);
+    }
+
+    public function aksi_ubah()
+    {
+        // $this->input->post digunakan untuk mengambil data inputan view dari name
+        $nimlama = $this->input->post('nimlama');
+        $nimbaru = $this->input->post('nimbaru');
+        $nama = $this->input->post('nama');
+
+        // query sql $this->db->query(" isi_query ");
+        $this->db->query("UPDATE mahasiswa SET nim = '$nimbaru' , nama = '$nama' WHERE nim = '$nimlama' ");
+
+        echo "<script>alert('data berhasil di ubah ');</script>";
+
+        // menuju alamat crud
+        redirect('crud');
+    }
+
+    public function hapus($nim)
+    {
+
+         $this->db->query("DELETE FROM mahasiswa WHERE nim = '$nim' ");
+
+        // menuju alamat crud
+        redirect('crud');
     }
 }
---
+++ application/views/crud/tambah.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <div class="container">
+        <h5 class=" mt-3">Form Tambah Data Nama_tabel</h5>
+        <form action="<?= base_url('crud/aksi_tambah') ?>" method="post" enctype="multipart/form-data">
+            <div class="mb-3 row">
+                <label for="nim" class="col-sm-2 col-form-label">Nim</label>
+                <div class="col-sm-10">
+                    <!-- inputan di tampung dengan name="nim" dan akan di teruskan ke link action -->
+                    <input type="text" class="form-control" id="nim" name="nim">
+                </div>
+            </div>
+            <div class="mb-3 row">
+                <label for="nama" class="col-sm-2 col-form-label">nama</label>
+                <div class="col-sm-10">
+
+                    <!-- inputan di tampung dengan name="nama"  dan akan di teruskan ke link action -->
+                    <input type="text" class="form-control" id="nama" name="nama">
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary mt-3">SIMPAN</button>
+        </form>
+    </div>
+</body>
+
+</html>
